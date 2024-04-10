d598c37017954867f33523188b15766ce93baeea
Elmacflai <arquitectoalfonso@gmail.com>
2024-04-09 22:40:27 -0600

    se agrego pikachu al juego

--- pokemon.js
+++ pokemon.js
@@ -6,7 +6,11 @@ let squirtle = {
     name : "squirtle",
     tipo : "agua"
 }
-
+let pikachu = {
+    name : "pikachu",
+    tipo : "electrico"
+}
+/*
 if (charmander.tipo === "fuego" ) {
     console.log("Charmander es mas fuerte que squirtle")
 
@@ -14,4 +18,11 @@ if (charmander.tipo === "fuego" ) {
     console.log("Charmander no es mas fuerte que squirtle")
 
 }
+*/
+if (pikachu.tipo === "electrico" ) {
+    console.log("Pikachu le gana a squirtle")
 
+} else {
+    console.log("Pikachu pierde contra squirtle")
+
+}
