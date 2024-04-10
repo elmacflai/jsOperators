e4057f69292b1ccb9cc80cf73eb09f381401165f
Elmacflai <arquitectoalfonso@gmail.com>
2024-04-09 22:54:56 -0600

    killing entro al chat

--- dragonBallZ.js
+++ dragonBallZ.js
@@ -2,10 +2,22 @@ let goku = {
     powerlvl : 9000, 
 }
 
+let krilling = {
+    powerlvl : 3000, 
+}
+
 if (goku.powerlvl >= 8000 ) {
     console.log("Goku mata a freezer")
 
 } else {
     console.log("Goku muere de nuevo")
+
+}
+
+if (krilling.powerlvl >= 8000 ) {
+    console.log("is over 9000! break scooter")
+
+} else {
+    console.log("Goku haaaaa!")
 
 }
