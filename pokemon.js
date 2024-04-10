0618716d72f5635dae7ee9b610ac5ec7a1a946e4
Elmacflai <arquitectoalfonso@gmail.com>
2024-04-09 22:36:10 -0600

    juego de pokemon

---
+++ pokemon.js
@@ -0,0 +1,17 @@
+let charmander = {
+    name : "charmander", 
+    tipo : "fuego"
+}
+let squirtle = {
+    name : "squirtle",
+    tipo : "agua"
+}
+
+if (charmander.tipo === "fuego" ) {
+    console.log("Charmander es mas fuerte que squirtle")
+
+} else {
+    console.log("Charmander no es mas fuerte que squirtle")
+
+}
+
